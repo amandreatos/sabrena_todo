28df98b78dde499ce6d329b72d50e0353f588fe6
Anthony Andreatos <amandreatos@yahoo.com>
2018-08-01 18:36:43 -0400

    ready for pull

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,20 +2,30 @@ import React from "react";
 
 const TodoList = ({ items, action, done }) => {
   let lis = [];
-  let completionStatus = done === false ? 'Todo' : 'Done'
-  let completionColor = done === false ? 'green' : 'red';
+  let completionStatus = done === false ? "Todo" : "Done";
+  let completionColor = done === false ? "green" : "red";
   let mark = done === false ? "\u2713" : "x";
   for (let i in items) {
     if (items[i].completed === done) {
-      lis.push(<div className="todoItem" key={i}>
+      lis.push(
+        <div className="todoItem" key={i}>
           <span>{items[i].item}</span>
-          <span class="status" style={{backgroundColor: completionColor}} onClick={() => action(i)}>{mark}</span>
-        </div>);
+          <span
+            class="status"
+            style={{ backgroundColor: completionColor }}
+            onClick={() => action(i)}
+          >
+            {mark}
+          </span>
+        </div>
+      );
     }
   }
-  return <div className="todoItems"> 
-  <h4>{completionStatus}</h4>
-  {lis} 
-  </div>;
+  return (
+    <div className="todoItems">
+      <h4>{completionStatus}</h4>
+      {lis}
+    </div>
+  );
 };
 export default TodoList;
